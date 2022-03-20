6a653ea5cfd42f3eea54be93c8b3b88d68717197
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2022-03-20 12:14:37 -0300

    Mejora en el renderizado de los  proyectos y estilos globales

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,12 +2,7 @@ import React, { useState } from "react";
 import SectionTitle from "./SectionTitle";
 import styles from "../styles/Projects.module.css";
 import { Project } from "./Project";
-import imgPaginaPersonal from '../img/pagina-personal.webp'
-import imgWebEnBuscaDeLaSantidad from '../img/enbuscadelasantidad-web.webp'
-import imgJuegoEnBuscaDeLaSantidad from '../img/enbuscadelasantidad-juego.webp'
-import imgVSM from '../img/vsm.webp'
-import imgClimApp from '../img/app-clima.webp'
-import imgTusLinks from '../img/tus-links.webp'
+import projects from './projects.json'
 
 export const Projects = () => {
   const [category, setCategory] = useState("all");
@@ -25,109 +20,29 @@ export const Projects = () => {
       </div>
 
       <div className={styles.content}>
-        {category == "web" ?
-          <>
-            <Project
-              title="Página web personal"
-              description="Página web personal donde se podrá encontrar infomación sobre mí, estudios, tecnologías utilizadas, proyectos realizados y cv. La misma fue realizada utilizando React."
-              github="https://github.com/alecalgaro/Alejandro-Calgaro"
-              web="https://alejandrocalgaro.netlify.app/"
-              url_img={imgPaginaPersonal}
-            />
-
-            <Project
-              title="Página web - En busca de la santidad"
-              description="Página web para el videojuego 'En busca de la santidad', creado para la Infancia y Adolescencia Misionera. Fue realizada utilizando HTML, CSS, Bootstrap y JS."
-              github="https://github.com/alecalgaro/enbuscadelasantidad"
-              web="https://enbuscadelasantidad.com.ar/"
-              url_img={imgWebEnBuscaDeLaSantidad}
-            />
-
-            <Project
-              title="Tus links"
-              description="Aplicación web creada como práctica de React y Firebase. La misma permite ingresar un titulo, descripción, categoría y link para guardarlos en una base de datos y poder acceder a ellos."
-              github="https://github.com/alecalgaro/tus-links"
-              url_img={imgTusLinks}
-            />
-          </>
-
-            : (category == "movil") ?
-                <>
-                    <Project
-                        title="ClimApp"
-                        description="ClimApp es una aplicación del clima creada en React Native para el proyecto grupal del curso de especialización en desarrollo mobile realizado en conjunto por IBM y Codo a Codo."
-                        github="https://github.com/alecalgaro/App-Clima-IBM"
-                        url_img={imgClimApp}
-                    />
-                </>
-
-            : (category == "games") ?
-                <>
-                    <Project
-                        title="Videojuego - En busca de la santidad"
-                        description="Videojuego 'En busca de la santidad', creado para la Infancia y Adolescencia Misionera. El mismo puede ser jugado de forma online o descargándolo en dispositivos móviles android y computadoras."
-                        web="http://enbuscadelasantidad.itch.io/juego"
-                        url_img={imgJuegoEnBuscaDeLaSantidad}
-                    />
-                </>
-
-            : (category == "desktop") ?
-                <>
-                    <Project
-                    title="VSM"
-                    description="Software destinado a la gestión de viviendas sociales en madera y automatización de la tarea de diseño de las mismas en AutoCAD. Contiene una interfaz gráfica desarrollada en Java y la automatización en AutoLISP."
-                    github="https://github.com/alecalgaro/vsm"
-                    url_img={imgVSM}
-                    />
-                </>
-
-            :   // todos:
-            <>
-                <Project
-                    title="Página web personal"
-                    description="Página web personal donde se podrá encontrar infomación sobre mí, estudios, tecnologías utilizadas, proyectos realizados y cv. La misma fue realizada utilizando React."
-                    github="https://github.com/alecalgaro/Alejandro-Calgaro"
-                    web="https://alejandrocalgaro.netlify.app/"
-                    url_img={imgPaginaPersonal}
-                />
-
-                <Project
-                    title="Página web - En busca de la santidad"
-                    description="Página web para el videojuego 'En busca de la santidad', creado para la Infancia y Adolescencia Misionera. Fue realizada utilizando HTML, CSS, Bootstrap y JS."
-                    github="https://github.com/alecalgaro/enbuscadelasantidad"
-                    web="https://enbuscadelasantidad.com.ar/"
-                    url_img={imgWebEnBuscaDeLaSantidad}
-                />
-
-                <Project
-                    title="Videojuego - En busca de la santidad"
-                    description="Videojuego 'En busca de la santidad', creado para la Infancia y Adolescencia Misionera. El mismo puede ser jugado de forma online o descargándolo en dispositivos móviles android y computadoras."
-                    web="http://enbuscadelasantidad.itch.io/juego"
-                    url_img={imgJuegoEnBuscaDeLaSantidad}
-                />
-
-                <Project
-                    title="VSM"
-                    description="Software destinado a la gestión de viviendas sociales en madera y automatización de la tarea de diseño de las mismas en AutoCAD. Contiene una interfaz gráfica desarrollada en Java y la automatización en AutoLISP."
-                    github="https://github.com/alecalgaro/vsm"
-                    url_img={imgVSM}
-                />
-
-                <Project
-                    title="ClimApp"
-                    description="ClimApp es una aplicación del clima creada en React Native para el proyecto grupal del curso de especialización en desarrollo mobile realizado en conjunto por IBM y Codo a Codo."
-                    github="https://github.com/alecalgaro/App-Clima-IBM"
-                    url_img={imgClimApp}
-                />
-
-                <Project
-                    title="Tus links"
-                    description="Aplicación web creada como práctica de React y Firebase. La misma permite ingresar un titulo, descripción, categoría y link para guardarlos en una base de datos y poder acceder a ellos."
-                    github="https://github.com/alecalgaro/tus-links"
-                    url_img={imgTusLinks}
-                />
-            </>
-    }
+        { category != "all" ?
+            projects.filter(project => project.category == category).map((project) => (
+              <Project
+                key={project.id_project}
+                title={project.title}
+                description={project.description}
+                github={project.github}
+                web={project.web}
+                url_img={"https://drive.google.com/uc?export=view&id="+project.img}   
+              />                       
+            )) 
+          :
+            projects.map((project) => (
+              <Project
+                key={project.id_project}
+                title={project.title}
+                description={project.description}
+                github={project.github}
+                web={project.web}
+                url_img={"https://drive.google.com/uc?export=view&id="+project.img}   
+            />                       
+            ))
+        }
       </div>
     </section>
   );
