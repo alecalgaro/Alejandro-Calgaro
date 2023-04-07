f9c994db58264210ab6cf5429f8221b75042ced4
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2023-04-06 21:57:09 -0300

    Agregar nuevas tecnologías

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -4,6 +4,7 @@ import styles from "../styles/Technologies.module.css";
 import iconHTML from "../img/icons/icon-html.webp";
 import iconCSS from "../img/icons/icon-css.webp";
 import iconBootstrap from "../img/icons/icon-bootstrap.webp";
+import iconTailwind from "../img/icons/icon-tailwind.webp";
 import iconJS from "../img/icons/icon-js.webp";
 import iconReact from "../img/icons/icon-react.webp";
 import iconJava from "../img/icons/icon-java.webp";
@@ -12,6 +13,7 @@ import iconMySQL from "../img/icons/icon-mysql.webp";
 import iconFirebase from "../img/icons/icon-firebase.webp";
 import iconGit from "../img/icons/icon-git.webp";
 import iconGithub from "../img/icons/icon-github.webp";
+import iconPlaywright from "../img/icons/icon-playwright.webp";
 import iconFigma from "../img/icons/icon-figma.webp";
 import Technologie from "./Technologie";
 
@@ -23,6 +25,7 @@ export const Technologies = () => {
 				<Technologie icon={iconHTML} alt="icon-html" name="HTML" />
 				<Technologie icon={iconCSS} alt="icon-css" name="CSS" />
 				<Technologie icon={iconBootstrap} alt="icon-bootstrap" name="BOOTSTRAP" />
+				<Technologie icon={iconTailwind} alt="icon-tailwind" name="TAILWIND" />
 				<Technologie icon={iconJS} alt="icon-js" name="JS" />
 				<Technologie icon={iconReact} alt="icon-react" name="REACT" />
 				<Technologie icon={iconJava} alt="icon-java" name="JAVA" />
@@ -31,6 +34,7 @@ export const Technologies = () => {
 				<Technologie icon={iconFirebase} alt="icon-firebase" name="FIREBASE" />
 				<Technologie icon={iconGit} alt="icon-git" name="GIT" />
 				<Technologie icon={iconGithub} alt="icon-github" name="GITHUB" />
+				<Technologie icon={iconPlaywright} alt="icon-playwright" name="PLAYWRIGHT" />
 				<Technologie icon={iconFigma} alt="icon-figma" name="FIGMA" />
 			</div>
 		</section>
