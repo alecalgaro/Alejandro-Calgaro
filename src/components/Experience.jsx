76e379eb3eb6769ddf29eb41b2d6836fb3f689ee
Alejandro Calgaro <alejandrocalgaro@gmail.com>
2024-01-20 17:13:55 -0300

    Modificar texto de experiencia y hover de iconos tecnologías

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,7 +8,7 @@ export const Experience = () => {
 			<SectionTitle title="Experiencia" />
 			<div className={styles.content}>
 				<p className={styles.year}>2023 - Actualmente</p>
-				<p className={styles.title}>Ayudante Alumno</p>
+				<p className={styles.title}>Docencia - Ayudante Alumno</p>
 				<p className={styles.description}>
 					Cátedra Proyecto Final de Carrera de Analista en Informática Aplicada (FICH - UNL).
 				</p>
